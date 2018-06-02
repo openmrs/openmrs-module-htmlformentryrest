86094247f1cfcf3f65a4af99da3b70113c9bd9ab
Owais Ahmed Hussain <owais.a.hussain@gmail.com>
2018-06-02 18:28:47 +0500

    - Successful response from http://localhost:8080/openmrs/ws/rest/v1/htmlformentryrest/htmlform - Started writing HtmlFormResource - Added Htmlformentry as dependency

--- omod/src/main/java/org/openmrs/module/htmlformentryrest/web/controller/HtmlFormResource.java
+++ omod/src/main/java/org/openmrs/module/htmlformentryrest/web/controller/HtmlFormResource.java
@@ -9,9 +9,15 @@
  */
 package org.openmrs.module.htmlformentryrest.web.controller;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.htmlformentry.HtmlForm;
+import org.openmrs.module.htmlformentry.HtmlFormEntryService;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.RefRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
@@ -20,44 +26,56 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 /**
  * @author owais.hussain@ihsinformatics.com
  */
-@Resource(name = RestConstants.VERSION_1 + "/htmlformentryrest/htmlform", supportedClass = DummyHtmlForm.class, supportedOpenmrsVersions = {
+@Resource(name = RestConstants.VERSION_1 + "/htmlformentryrest/htmlform", supportedClass = HtmlForm.class, supportedOpenmrsVersions = {
         "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
-public class HtmlFormResource extends DelegatingCrudResource<DummyHtmlForm> {
+public class HtmlFormResource extends DelegatingCrudResource<HtmlForm> {
 	
 	@Override
-	public DummyHtmlForm newDelegate() {
-		// TODO Auto-generated method stub
-		return null;
+	public HtmlForm newDelegate() {
+		return new HtmlForm();
 	}
 	
 	@Override
-	public DummyHtmlForm save(DummyHtmlForm delegate) {
-		// TODO Auto-generated method stub
-		return null;
+	public HtmlForm save(HtmlForm delegate) {
+		return Context.getService(HtmlFormEntryService.class).saveHtmlForm(delegate);
 	}
 	
 	@Override
-	public DummyHtmlForm getByUniqueId(String uniqueId) {
-		// TODO Auto-generated method stub
-		return null;
+	public HtmlForm getByUniqueId(String uniqueId) {
+		return (HtmlForm) Context.getService(HtmlFormEntryService.class).getItemByUuid(HtmlForm.class, uniqueId);
 	}
 	
 	@Override
-	protected void delete(DummyHtmlForm delegate, String reason, RequestContext context) throws ResponseException {
-		// TODO Auto-generated method stub
-		
+	protected void delete(HtmlForm delegate, String reason, RequestContext context) throws ResponseException {
+		// TODO
 	}
 	
 	@Override
-	public void purge(DummyHtmlForm delegate, RequestContext context) throws ResponseException {
-		// TODO Auto-generated method stub
-		
+	public void purge(HtmlForm delegate, RequestContext context) throws ResponseException {
+		Context.getService(HtmlFormEntryService.class).purgeHtmlForm(delegate);
 	}
 	
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-		// TODO Auto-generated method stub
-		return null;
+		DelegatingResourceDescription description = null;
+		
+		if (rep instanceof RefRepresentation) {
+			description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("display");
+			description.addSelfLink();
+			return description;
+		} else if (rep instanceof DefaultRepresentation || rep instanceof FullRepresentation) {
+			description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("name");
+			description.addProperty("xmlData");
+			description.addProperty("description");
+			description.addSelfLink();
+			if (rep instanceof DefaultRepresentation) {
+				description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			}
+		}
+		return description;
 	}
-	
 }
